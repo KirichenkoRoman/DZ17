01dcf25edb4a0cbab0aa7c5d662bce0ca9cb7726
KirichenkoRoman <94745952+KirichenkoRoman@users.noreply.github.com>
2021-12-24 17:53:02 +0200

    Add files via upload

---
+++ Stick.js
@@ -0,0 +1,19 @@
+var x = 9, y = 3;
+
+alert("multiplied: " + x*y);
+
+alert("added: " + (x+y));
+
+alert("take away:" + (x-y)); 
+
+    var h = 12;
+    alert("Первое значение: " + h);
+
+    alert("Второе значение: " + h**2);
+    
+
+   alert("156" - 29); 
+   
+   alert("254" - 928); 
+    
+   alert("623" - 452); 
